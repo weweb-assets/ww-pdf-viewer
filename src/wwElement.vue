1d351246b6dca17cd6a9ffa4c15497c5f96e1425
Kévin Barfleur <39409397+kevinbarfleur@users.noreply.github.com>
2022-06-07 13:40:11 +0200

    v1 pdf

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,28 +1,68 @@
 <template>
-  <div class="my-element">
-    <p :style="textStyle">I am a custom element !</p>
-  </div>
+  <div
+    ref="pdf"
+    class="ww-pdf-viewer"
+    :class="{ editing: isEditing }"
+    :key="componentKey"
+  />
 </template>
 
 <script>
+import PDFObject from "pdfobject";
+
 export default {
   props: {
     content: { type: Object, required: true },
+    /* wwEditor:start */
+    wwEditorState: { type: Object, required: true },
+    /* wwEditor:end */
+  },
+  watch: {
+    content: {
+      deep: true,
+      handler() {
+        this.init();
+        console.log("toto");
+      },
+    },
   },
   computed: {
-    textStyle() {
-      return {
-        color: this.content.textColor,
-      };
+    isEditing() {
+      /* wwEditor:start */
+      return (
+        this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION
+      );
+      /* wwEditor:end */
+      // eslint-disable-next-line no-unreachable
+      return false;
     },
+  },
+  methods: {
+    init() {
+      console.log("init");
+      console.log(this.content.defaultZoom);
+      const container = this.$refs.pdf;
+      const options = {
+        pdfOpenParams: {
+          zoom: `${this.content.defaultZoom}`,
+          page: `${this.content.defaultPage}`,
+        },
+      };
+      PDFObject.embed(this.content.file, container, options);
+    },
+  },
+  mounted() {
+    this.init();
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.my-element {
-  p {
-    font-size: 18px;
+.ww-pdf-viewer {
+  width: inherit;
+  height: inherit;
+  &.editing {
+    pointer-events: none;
   }
 }
 </style>
--- ww-config.js
+++ ww-config.js
@@ -1,16 +1,45 @@
 export default {
   editor: {
     label: {
-      en: "My Element",
+      en: "PDF viewer",
     },
   },
   properties: {
-    textColor: {
-      label: {
-        en: "Text color",
+    file: {
+      label: { en: "PDF Url", fr: "PDF Url" },
+      type: "Text",
+      options: {
+        placeholder: "PDF url",
       },
-      type: "Color",
-      defaultValue: "#F23636",
+      defaultValue:
+        "https://weweb-staging.s3.amazonaws.com/designs/1fb3c298-9f4f-4b49-9661-35a8eee7941b/files/Focussurcatnat.pdf",
+      bindable: true,
+    },
+    defaultZoom: {
+      type: "Number",
+      label: {
+        en: "Default zoom",
+        fr: "zoom par defaut",
+      },
+      options: {
+        min: 1, // default 0
+        max: 1000, // default 100,
+        step: 1, // default 1
+      },
+      defaultValue: 100,
+    },
+    defaultPage: {
+      type: "Number",
+      label: {
+        en: "Default page",
+        fr: "Page par defaut",
+      },
+      options: {
+        min: 1, // default 0
+        max: 1000, // default 100,
+        step: 1, // default 1
+      },
+      defaultValue: 1,
     },
   },
 };
