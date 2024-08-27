727be11a70bb8be87f4b62cbd22eb1f9f049148a
Aurélie Violette <orwel.violette@gmail.com>
2024-08-27 11:17:12 +0200

    Fix selection on the editor

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -1,74 +1,73 @@
 <template>
-  <div
-    class="ww-pdf-viewer"
-    :class="{ editing: isEditing }"
-    ref="pdfContainer"
-  ></div>
+    <div class="ww-pdf-viewer" :class="{ editing: isEditing }" ref="pdfContainer"></div>
 </template>
 
 <script>
 import PDFObject from "pdfobject";
 
 export default {
-  props: {
-    content: { type: Object, required: true },
-    /* wwEditor:start */
-    wwEditorState: { type: Object, required: true },
-    /* wwEditor:end */
-  },
-  data() {
-    return {
-      resizeObserver: null,
-    };
-  },
-  watch: {
-    content: {
-      deep: true,
-      handler() {
-        this.init();
-      },
+    props: {
+        content: { type: Object, required: true },
+        /* wwEditor:start */
+        wwEditorState: { type: Object, required: true },
+        /* wwEditor:end */
     },
-  },
-  computed: {
-    isEditing() {
-      /* wwEditor:start */
-      return (
-        this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION
-      );
-      /* wwEditor:end */
-      // eslint-disable-next-line no-unreachable
-      return false;
+    data() {
+        return {
+            resizeObserver: null,
+        };
     },
-  },
-  methods: {
-    initObserver() {
-      this.resizeObserver = new ResizeObserver(() => {
-        this.init();
-      });
+    watch: {
+        content: {
+            deep: true,
+            handler() {
+                this.init();
+            },
+        },
+    },
+    computed: {
+        isEditing() {
+            /* wwEditor:start */
+            return this.wwEditorState.editMode === wwLib.wwEditorHelper.EDIT_MODES.EDITION;
+            /* wwEditor:end */
+            // eslint-disable-next-line no-unreachable
+            return false;
+        },
+    },
+    methods: {
+        initObserver() {
+            this.resizeObserver = new ResizeObserver(() => {
+                this.init();
+            });
 
-      this.resizeObserver.observe(this.$el);
+            this.resizeObserver.observe(this.$el);
+        },
+        clearObserver() {
+            this.resizeObserver.disconnect();
+        },
+        init() {
+            PDFObject.embed(this.content.pdf, this.$refs.pdfContainer);
+        },
     },
-    clearObserver() {
-      this.resizeObserver.disconnect();
+    beforeUnmount() {
+        this.resizeObserver.disconnect();
     },
-    init() {
-      PDFObject.embed(this.content.pdf, this.$refs.pdfContainer);
+    mounted() {
+        this.init();
+        this.initObserver();
     },
-  },
-  beforeUnmount() {
-    this.resizeObserver.disconnect();
-  },
-  mounted() {
-    this.init();
-    this.initObserver();
-  },
 };
 </script>
 
 <style lang="scss" scoped>
+/* wwEditor:start */
 .ww-pdf-viewer {
-  &.editing {
-    pointer-events: none;
-  }
+    position: relative;
+    &.editing::after {
+        content: "";
+        position: absolute;
+        inset: 0;
+    }
 }
+/* wwEditor:end */
 </style>
